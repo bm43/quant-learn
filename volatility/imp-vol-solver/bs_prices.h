f258de4f319692d10b626b813d161b6cbde0afad
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-03-25 10:29:24 +0900

    changed norm_cdf

--- volatility/imp-vol-solver/bs_prices.h
+++ volatility/imp-vol-solver/bs_prices.h
@@ -16,14 +16,7 @@ double norm_pdf(const double x) {
 }
 
 double norm_cdf(const double x) {
-  double k = 1.0/(1.0 + 0.2316419*x);
-  double k_sum = k*(0.319381530 + k*(-0.356563782 + k*(1.781477937 + k*(-1.821255978 + 1.330274429*k))));
-
-  if (x >= 0.0) {
-    return (1.0 - (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x) * k_sum);
-  } else {
-    return 1.0 - norm_cdf(-x);
-  }
+  return 0.5 * std::erfc(-x * M_SQRT1_2);
 }
 
 // components of the black scholes solution:
