4038f398b7afedb97701ee38f8f72dfa3817370b
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-03-31 22:54:03 +0900

    Update CRRoption.cpp

--- pricers/CRRoption.cpp
+++ pricers/CRRoption.cpp
@@ -1,3 +1,12 @@
+#include "CRRoption.h"
+#include "binomial.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
 int GetInputData(int& N, double& K) {
+    cout << "Enter steps to expiry N: "; cin >> N;
+    cout << "Enter strike price K: "; cin >> K;
+    cout << endl;
     return 0;
 }
