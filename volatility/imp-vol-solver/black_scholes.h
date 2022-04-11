eed4ae10698e95618acd901cd49cfcf2d8de10b8
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-04-11 14:09:21 +0900

    Update black_scholes.h

--- volatility/imp-vol-solver/black_scholes.h
+++ volatility/imp-vol-solver/black_scholes.h
@@ -14,4 +14,20 @@ class BlackScholesCall {
         double _r, double _T);
         double operator()(double sigma) const;
 };
+
+class BlackScholesPut {
+
+    private:
+        double S; // underlying asset price cuz its option
+        double K; //strike price
+        double r; // risk-free rate
+        double T; // time to maturity
+
+    public:
+        BlackScholesPut(double _S, double _K,
+        double _r, double _T);
+        double operator()(double sigma) const;
+};
+
+
 #endif
