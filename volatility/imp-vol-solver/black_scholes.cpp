3fe8cab37a66e4bfea8cd296a89a9664c8e39fb0
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-04-11 14:08:40 +0900

    Update black_scholes.cpp

--- volatility/imp-vol-solver/black_scholes.cpp
+++ volatility/imp-vol-solver/black_scholes.cpp
@@ -12,4 +12,13 @@ BlackScholesCall::BlackScholesCall(double _S, double _K, double _r, double _T): 
 double BlackScholesCall::operator()(double sigma) const {
     return call_price(S,K,r,sigma,T);
 }
+
+BlackScholesPut::BlackScholesPut(double _S, double _K, double _r, double _T): S(_S), K(_K), r(_r), T(_T) {
+
+}
+
+double BlackScholesPut::operator()(double sigma) const {
+    return put_price(S,K,r,sigma,T);
+}
+
 #endif
