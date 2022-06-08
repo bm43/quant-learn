7b2b0ed2602a87d960e23629b95a779553e8d7d3
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-06-09 00:18:20 +0900

    Update CRRoption.h

--- pricers/options/CRRoption.h
+++ pricers/options/CRRoption.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #ifndef CRROPTION_H
 #define CRROPTION_H
 
@@ -24,5 +24,4 @@ double PriceByCRR(double S0, double U, double D, double R, int N, double K);
 //computing call payoff
 double CallPayoff(double z, double K);
 
->>>>>>> d2b34c2a0baed62724d4653b460d67ca50e1a50e
 #endif
