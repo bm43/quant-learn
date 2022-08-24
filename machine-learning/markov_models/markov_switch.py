79f71547a24e9ada7bf484166076b8b87c3f50c8
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-08-24 17:55:59 +0900

    adding stuff

--- machine-learning/markov_models/markov_switch.py
+++ machine-learning/markov_models/markov_switch.py
@@ -10,14 +10,33 @@ https://econweb.ucsd.edu/~jhamilto/palgrav1.pdf
   https://homepage.ntu.edu.tw/~ckuan/pdf/Lec-Markov_note.pdf
 """
 
+import import random
 import numpy as np
+
+import pandas as pd
 from scipy.optimize import minimize
 from scipy.stats import norm
 from dataclasses import dataclass, field
-from typing import 
 
 @dataclass
-class MarkovSwitchModel:
+class MarkovSwitch:
   
+  n_regime: int = 2
+
   def _sigmoid(self, x: np.ndarray) -> np.ndarray:
     return 1/(1+np.exp(-x))
+
+  def _em(self, obs: np.ndarray, iter: int = 10):
+    n = obs.shape[0]
+    n_regime = self.n_regime
+
+
+    idx = np.random.randint(low=0, high=n, size=n_regime)
+    mu_k = obs[idx] # means for regimes
+    p_k = np.zeros(n_regime)
+    theta = [0] * iter
+    sig = np.ones(1)
+
+    for i in range(iter):
+      theta[i]
+  def fit(self, obs: np.ndarray, iter: int = 10):
--- pricers/contracts.py
+++ pricers/contracts.py
@@ -4,5 +4,7 @@
 These contracts combine a guaranteed payoff with a bonus equal to
 some accumulation of the capped periodic returns of a reference portfolio
 """
+# https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1101796
 import numpy as np
 from dataclasses import dataclass, field
+
