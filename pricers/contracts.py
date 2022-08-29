2737475fc401fb58004f22a907a63f2638fd8893
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-08-29 17:01:17 +0900

    adding stuff

--- pricers/contracts.py
+++ pricers/contracts.py
@@ -1,12 +1,24 @@
-# https://sci.bban.top/pdf/10.1137/100818157.pdf#view=FitH
-# locally capped contracts:
 """
-These contracts combine a guaranteed payoff with a bonus equal to
+implementing:
+1. locally capped contracts pricer
+2. cliquet option pricer
+in a black-scholes market,
+
+based on the following paper:
+https://sci.bban.top/pdf/10.1137/100818157.pdf#view=FitH
+
+Locally capped contract:
+these contracts combine a guaranteed payoff with a bonus equal to
 some accumulation of the capped periodic returns of a reference portfolio
+
+explanations on this product with some math:
+https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1101796
 """
-# https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1101796
+
+from attr import dataclass
 import numpy as np
 from scipy.stats import norm
+from typing import field, Union
 
 """
 notes:
@@ -20,11 +32,19 @@ time period from 0 to T divided by n
 t_1 = T/n, t_2 = 2T/n ... t_n = T
 
 valuation of this product can be done using black scholes
-
 """
+
 # rough plan:
 
-# import relative path to use .hpp file?
+######################################
+# I. globally capped contract pricer #
+######################################
+
+"""
+Globally capped contracts are replicated by a portfolio
+containing two call options
+Indeed, price of gcc depends on the price diff of those
+2 options."""
 
 def d_j(j: int, S: float, K: float, r: float, sigma: float, T: float):
   return (np.log(S/K) + (r + ( (-1)**(j-1) )*0.5*sigma**2)*T) / (sigma*((T**0.5)))
@@ -33,23 +53,55 @@ def d_j(j: int, S: float, K: float, r: float, sigma: float, T: float):
 def C(S: float, K: float, r: float, sigma: float, T: float):
     return S * norm.cdf(d_j(1, S, K, r, sigma, T)) - K * np.exp(-r*T) * norm.cdf(d_j(2, S, K, r, sigma, T))
 
-"""
-Globally capped contracts are replicated by a portfolio
-containing two call options
-Indeed, price of gcc depends on the price diff of those
-2 options."""
 def GloballyCappedContractPricer(c: float, g: float, S: float,\
     r: float, T: float):
     return (1+g)*np.exp(-r*T) + (1/S) *( C(S*(1+g)) - C(S*(1+c)))
+
+#######################################
+# II. locally-capped contract pricer  #
+#######################################
 
 """
 There is no closed-form solution
 for price of locally-capped contracts.
 So we can use monte carlo simulation
-or find semi-closed form
+or find semi-closed form.
+
+In this case, we derive a semi-closed form of
+the monthly sum cap's price.
 """
 
-def _Q(R: float): # describes distribution of C_k
+@dataclass
+class MonthlySumCapPrice:
+    
+    S: np.array # underlying asset prices from t_0 to T
+
+    r: float = field(init=False, default=0.03) # risk free rate
+    g: float = field(init=False, default=0.03) # guaranteed rate at T
+    T: float = field(init=False, default=3.0) # maturity in years
+    delta: float = field(init=False) # time interval btwn price ticks
+    sig: float = field(init=False) # volatility of S
+    eta: float = field(init=False) # constant yield of dividend of designated reference equity index
+
+    def __post_init__(self) -> None:
+        """note: assuming clean data
+        i.e. regular time intervals between price ticks
+        up to maturity T
+        """
+        n = self.S.shape[0] # length of price data
+        self.delta = self.T/n # tk - tk-1 = delta
+        self.sig = self.S.std
+
+
+
+    def _risk_neutral_prob(self, r: float, eta: float, sig: float, delta: float) -> norm:
+        # cumulative normal dist with
+        # mean = (r - eta - ((sig**2)/2) * delta)
+        # std = (sig**2) * delta
+        return norm(loc = r - eta - ((sig**2)/2) * delta, scale = (sig**2) * delta)
+
+
+def _risk_neutral_prob(R: float): # describes distribution of C_k
     return
 
 def phi_C(g: float, c: float): # characteristic function of C_k
