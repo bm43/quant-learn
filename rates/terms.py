1af0c8e71af44e50326ea81a5f1200fafbd5669e
Hyung Jip LEE <hyungjip777@gmail.com>
2022-09-15 23:53:02 +0900

    build curve

--- rates/terms.py
+++ rates/terms.py
@@ -1,60 +1,53 @@
 # paper by patrick hagan, graeme west
 # methods for constructing yield curve
 
+from dataclasses import dataclass
 import pandas as pd
 import numpy as np
+from typing import Optional
 
+@dataclass
 class ZeroRateCurve:
     """Builds zero-rate curve using ZCBs with continuous discounting"""
 
     def __init__(
         self,
-        maturities: np.ndarray,
-        zcb: np.ndarray = None,
+        maturities: np.ndarray, # n_samples x 1
+        zcb: np.ndarray = None, # n_samples x 1
         zero_rates: np.ndarray = None,
     ):
         """Default Constructor used to initialize zero rate curve
-        :param maturities: maturity corresponding to each
-         zero coupon bond in the zcb array
-        :type maturities: np.ndarray
-         shape = (n_samples,)
-        :param zcb: zero coupon bond prices traded for various maturities
-        :type rates: np.ndarray
-         shape = (n_samples,)
+        maturities=  maturity corresponding to each zero coupon bond in the zcb array
+
+        zcb : zero coupon bond prices traded for various maturities
         """
         self.maturities = maturities
         self.zero_rates = zero_rates
         self.disc_factors = self.discount_factor(maturities, zero_rates)
         self.zcb_prices = self.disc_factors * 100.0
 
-    def discount_factor(self, t: float, rcont: float):
+    def discount_factor(self, t: float, rcont: float) -> float:
         """computes the discount factor corresponding to continuous rates
         This is the maximum amount you give up today to receive
         $1 in t years
-        Parameters
-        ----------
-        t : float
-            maturity of zcb
-        rcont : float
-            the continuous rate
-        Returns
-        -------
-        float
-            discount factor corresponding to continuous rate
+        
+        t = maturity of zcb
+        rcont = the continuous rate
+        
+        returns discount factor corresponding to continuous rate
         """
         return np.exp(-rcont * t)
 
-    def rates_from_discount_factors(self, t: float, discount_factor: float):
+    def rates_from_discount_factors(self, t: float, discount_factor: float) -> float:
         """computes the continuous rates from discount factors
         = rate you would receive if you bought the zcb today
         and held it until maturity t in years
         
         t : float = maturity of zcb or rate
         
-        :param discount_factor: zcb discount factor
-        :type discount_factor: float
-        :return: rate corresponding to discount factors
-        :rtype: float
+        discount_factor = zcb discount factor
+        
+        returns rate corresponding to discount factors
         """
         return (1.0 / t) * np.log(1 / discount_factor)
 
@@ -81,18 +74,11 @@ class ZeroRateCurve:
     # get cubic spline 
     def cubic_interp(self, t: float) -> float:
         return 0.0
-
-    def build_curve(self, fit_type: str = "linear") -> pd.Series:
+    
+    def build_curve(self, fit_type: Optional[str] = "linear_spot") -> pd.Series:
         """builds a zero rate curve based on type of interpolation
-        Parameters
-        ----------
-        fit_type : str, optional, default="linear_spot"
-            type of fit
-            supports one of linear_spot, constant_fwd, cubic_spline
-        Returns
-        -------
-        pd.Series
-            zero rate curve for all maturities
+        fit_type = linear_spot, constant_fwd, or cubic_spline
+        Returns zero rate curve for all maturities
         """
         tmax = self.maturities[-1]
         knot_points = np.arange(0, tmax, 0.01)
