312bb7c2337d0f2591c561f3d2c9c1a0b4ce5ee5
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-09-27 14:58:38 +0900

    fixed

--- pricers/options/merton_jump_diff.hpp
+++ pricers/options/merton_jump_diff.hpp
@@ -10,7 +10,7 @@ double merton_jump_diffusion_call(const double& S, const double& K, const double
                                         for (int k=1; k<it_num; ++k) {
                                             double r_k = r - lambda*(delta - 1) + (k*log(delta))/time_to_maturity;
                                             double sigma_k = sqrt(sigma*sigma + k*delta*delta/time_to_maturity);
-                                            option_price += ( exp(-delta*lambda*time_to_maturity) * pow(delta*lambda*time_to_maturity, k)/tgamma(k+1)) * black_scholes_call_price(S, K, r_k, sigma_k, time_to_maturity)
+                                            option_price += ( exp(-delta*lambda*time_to_maturity) * pow(delta*lambda*time_to_maturity, k)/tgamma(k+1)) * black_scholes_call_price(S, K, r_k, sigma_k, time_to_maturity);
                                         }
 
                                         return option_price;
