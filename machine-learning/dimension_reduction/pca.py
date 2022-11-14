b30ad1490dd74de58074a53119b5f05199dcac85
bm43 <hyungjip777@gmail.com>
2022-11-14 17:50:53 +0900

    finished code

--- machine-learning/dimension_reduction/pca.py
+++ machine-learning/dimension_reduction/pca.py
@@ -2,3 +2,26 @@
 # author: Hyung Jip Lee
 # 21 Oct. 2022
 
+import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class PCA:
+
+    n_components: int
+
+    def fit(self, X: np.ndarray):
+        
+        # get eigen val and vec
+        X_norm = X - np.mean(X, axis=0)
+        covar = np.cov(X_norm, rowvar = False)
+        self.eig, self.V = np.linalg.eigh(covar) # eig is eigenvalues, V is eigenvectors
+        
+        # sort the eigenvals and vecs in descending order
+        sort_idx = np.argsort(eig)[::-1]
+        self.eig = eig[sort_idx][:self.n_components]
+        self.V = V[:,sort_idx][:,0:self.n_components]
+
+        self.X_transformed = np.dot(self.eig.transpose(), X_norm.transpose()).transpose()
+
+        return self
