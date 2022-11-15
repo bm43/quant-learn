19e37acbf20180481b5193f3f4d7e5f59111b1f4
bm43 <hyungjip777@gmail.com>
2022-11-15 12:07:46 +0900

    c

--- DL_accelerate/pruning.py
+++ DL_accelerate/pruning.py
@@ -1,4 +1,8 @@
 import keras
+import torch
 
 # to be updated when code is ready
 # https://medium.com/@souvik.paul01/pruning-in-deep-learning-models-1067a19acd89
+
+# don't do from scratch,
+# try experiments using torch
