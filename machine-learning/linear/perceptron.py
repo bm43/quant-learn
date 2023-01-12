3ddc56bab7dc7b278ac6e51732e0d7bdb49a8774
bm43 <hyungjip777@gmail.com>
2023-01-12 17:52:10 +0900

    online

--- machine-learning/linear/perceptron.py
+++ machine-learning/linear/perceptron.py
@@ -1,3 +1,7 @@
+"""
+implementation of the perceptron learning algorithm
+author: Hyung Jip Lee"""
+
 import numpy as np
 import pandas as pd
 from dataclasses import dataclass
@@ -13,12 +17,25 @@ class Perceptron:
     degree: int = 1
 
     def fit(self, X: ndarray, y: ndarray):
-        
+        # online learning
         R = (np.sum(np.abs(X) ** 2, axis=-1) ** (0.5)).max()
-
+        bias = 0
+        deny = 0
+        prev_deny = 0
         # weight init
         self.thetas = np.zeros(X.shape[1]+1)
 
         # start training loop
+        while True:
+            for x_i, y_i in zip(X, y):
+                if y_i * (X @ self.thetas + bias) <= 0:
+                    self.thetas[1:] += self.eta * y_i * x_i
+                    bias += self.eta * y_i * R * R
+                    deny += 1
+            if prev_deny == deny:
+                break
+            prev_deny = deny
+        
+        self.thetas[0] = bias
 
         return self
