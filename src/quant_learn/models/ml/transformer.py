7982c3c2a4e8d378a123c37990aedbf7550fce97
Carl Lee <carllee@Carls-MacBook-Pro.local>
2026-01-17 16:59:50 +0900

    .

---
+++ src/quant_learn/models/ml/transformer.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+# dimensions:
+# input tokens = 1 x N list of tokens. (some text sequence, in this example)
+# including [BOS] and [EOS] tokens
+
+# 1 x N becomes N x d_k which are embeddings.
+# where d_k is dimension of the model (how many embeddings per token)
+# X = tokens^T x ? = (N x 1) x (1 x d_k) = N x d_k input embeddings
+
+# then what is this 1 x d_k vector?
+
+# then we multiply this X to get Q, K, and V:
+
+# Q = X x W_q = (N x d_k) x (d_k x d_k)
+# K = X x W_k = (N x d_k) x (d_k x d_k)
+# V = X x W_v = (N x d_k) x (d_k x d_k)
+
+def softmax(x: np.array):
+    return np.exp(x) / np.sum(np.exp(x))
+
+class Encoder():
+    def __init__(self, d_k:int = 384, n_h: int = 10):
+        self.d_k = d_k
+        self.n_h = n_h
+        
+        self.Q = np.random.randn(self.d_k, self.d_k)
+        self.K = np.random.randn(self.d_k, self.d_k)
+        self.V = np.random.randn(self.d_k, self.d_k)
+
+    
+    def forward(self):
+        return self
